cb46842aba77d5dc8a9e3bb8eb6e144ee81d75ce
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-10-24 20:46:54 +0700

    feat: soap change web param

--- src/src/main/java/org/Bondflix/service/Calculator.java
+++ src/src/main/java/org/Bondflix/service/Calculator.java
@@ -3,12 +3,13 @@ package org.Bondflix.service;
 
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
+import jakarta.jws.WebParam;
 
 @WebService
 public class Calculator {
 
     @WebMethod
-    public double add(double a, double b){
+    public double add(@WebParam(name ="arg1") double a, @WebParam(name = "arg2") double b){
         return a + b;
     }
 }
